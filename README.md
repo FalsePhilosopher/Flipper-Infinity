5c30a344dcb32d89f3b72f1ae73fa4e2b1cee346
FalsePhilosopher <joshgarland90@gmail.com>
2024-07-07 23:06:55 -0600

    updated converter script to include Key A/reconverted files

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Flipper-Infinity
 
-I slightly modified [Equipter's ClassicConverter](https://github.com/equipter/ClassicConverter) [7B_Converter.py](/Scripts/7B_Converter.py) for the conversion.  
-Run [infsha.py](/Scripts/infsha.py)`python3 infsha.py UID` to get the key A  
+I slightly modified Equipter's ClassicConverter https://github.com/equipter/ClassicConverter [7B_Converter.py](/Scripts/7B_Converter.py) for the conversion.  
 Edit the desired NFC file with the key A in these spots  
 ![Key A](Images/KeyA.png)  
 Transfer file to FZ and emulate.
--- Scripts/7B_Converter.py
+++ Scripts/7B_Converter.py
@@ -3,16 +3,40 @@ import os
 import pathlib
 from typing import Tuple
 import time
+import binascii
+import hashlib
+import re
+import sys
+
 # mifare classic mini DI 7b uid binary to flipper .nfc converter. 
 # Created by Equipter with code portions borrowed from Lucaslhm/AmiiboFlipperConverter
-# V1.07
-# Modified by FalsePhilosopher to work with DI toys
+# V1.08
+# Modified by FalsePhilospher to work with DI toys
 
+# Function from infsha.py
+uidre = re.compile('^04[0-9a-f]{12}$', re.IGNORECASE)
+magic_nums = [3, 5, 7, 23, 9985861487287759675192201655940647, 38844225342798321268237511320137937]
 
+def calc_keya(uid, sector):
+    if uidre.match(uid) is None:
+        raise ValueError('invalid UID (seven hex bytes)')
+
+    if sector < 0 or sector > 4:
+        raise ValueError('invalid sector (0-4)')
+
+    PRE = format(magic_nums[0] * magic_nums[1] * magic_nums[3] * magic_nums[5], '032x')
+    POST = format(magic_nums[0] * magic_nums[2] * magic_nums[4], '030x')
+
+    sha1 = hashlib.sha1()
+    sha1.update(binascii.unhexlify(PRE + uid + POST))
+    key = sha1.digest()
+
+    return binascii.hexlify(key[3::-1] + key[7:5:-1]).decode()
+
+# Functions from 7B_Converter.py
 def write_output(name: str, assemble: str, out_dir: str):
     with open(os.path.join(out_dir, f"{name}.nfc"), "wt") as f:
         f.write(assemble)
-
 
 def convert(contents: bytes) -> Tuple[str, int]:
     buffer = []
@@ -37,14 +61,12 @@ def convert(contents: bytes) -> Tuple[str, int]:
             Mf_size = 4
     return "\n".join(buffer), Block_count, Mf_size
 
-
 def get_uid(contents: bytes) -> str:
     Block = []
     for i in range(7):
         byte = contents[i : i + 1].hex()
         Block.append(byte)
     return " ".join(Block).upper()
-
 
 def get_sak(contents: bytes) -> str:
     Block = []
@@ -60,25 +82,26 @@ def get_atqa(contents: bytes) -> str:
         Block.append(atqa)
     return " ".join(Block).upper()
 
-
-def assemble_code(contents: {hex}) -> str:
+def assemble_code(contents: bytes) -> str:
     conversion, Block_count, Mf_size = convert(contents)
+    uid = get_uid(contents)
+    key_a = calc_keya(uid.replace(' ', ''), 0)  # Calculate key A
 
     return f"""Filetype: Flipper NFC device
 Version: 4
 # Nfc device type can be UID, Mifare Ultralight, Mifare Classic, Bank card
 Device type: Mifare Classic
 # UID, ATQA and SAK are common for all formats
-UID: {get_uid(contents)}
+UID: {uid}
 ATQA: 00 44
 SAK: 09
 # Mifare Classic specific data
 Mifare Classic type: MINI
 Data format version: 2
 # Mifare Classic Blocks, '??' means unknown data
+# Key A: {key_a}
 {conversion}
 """
-
 
 def convert_file(input_path: str, output_path: str):
     input_extension = os.path.splitext(input_path)[1]
@@ -88,20 +111,16 @@ def convert_file(input_path: str, output_path: str):
             name = os.path.split(input_path)[1]
             write_output(name.split(".bin")[0], assemble_code(contents), output_path)
 
-
-
 def process(path: str, output_path: str):
     if os.path.isfile(path):
         convert_file(path, output_path)
     else:
         for filename in os.listdir(path):
             new_path = os.path.join(path, filename)
-
-            if os.path.isfile(path):
-                convert_file(path, output_path)
+            if os.path.isfile(new_path):
+                convert_file(new_path, output_path)
             else:
                 process(new_path, output_path)
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -123,20 +142,19 @@ def get_args():
     args = parser.parse_args()
     return args
 
-
 def main():
     args = get_args()
 
     if os.path.isfile(args.input_path):
         if not args.output_path:
             args.output_path = os.path.split(args.input_path)[0]
-    
+
     os.makedirs(args.output_path, exist_ok=True)
     process(args.input_path, args.output_path)
-
 
 if __name__ == "__main__":
     main()
     print("Converting...")
     time.sleep(0.3)
     print("Completed Conversion")
+ 
